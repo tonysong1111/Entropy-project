195314a71c06c44a818d11d92bedb6c4da7c6adb
Tony <tonysong1111@gmail.com>
2020-07-09 22:50:13 -0400

    lyapunov features

--- entropyplot.py
+++ entropyplot.py
@@ -4,6 +4,8 @@ Tony Song, Entropy project
 Updated: July 2nd, 2020
 """
 import phasemaps as pm
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def cubicmap(coeffs, x):
@@ -22,8 +24,8 @@ def prompt_cubic():
     while line != "":
         p_list.append(tuple(map(float, line.split())))
         line = input()
-    if len(p_list) != 3:
-        print("you must enter exactly three coordinate pairs")
+    if len(p_list) != 2:
+        print("you must enter exactly two coordinate pairs")
         return -1
     return p_list
 
@@ -37,21 +39,43 @@ def prompt_config():
 
 
 if __name__ == "__main__":
-    p_list = prompt_cubic()
+    ### define model specifications ###
+    # p_list = prompt_cubic()
+    # coeffs = pm.generate_cubic(p_list)
+    # num_obj, num_iter, num_seq, width = prompt_config()
+    p_list = [(3.141, 2.356), (4.712, 1.385)]
     coeffs = pm.generate_cubic(p_list)
-    num_obj, num_iter, num_seq, width = prompt_config()
+    num_obj, num_iter, num_seq, width = (300, 150, 50, 2*np.pi)
 
     ### define cubic function ###
-    cubic = pm.Cubic(cubicmap)
-    cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
+    #cubic = pm.Cubic(cubicmap)
+    #cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
 
     ### print the lyapunov exponent ###
-    fstring = f"The average Lyapunov exponent is {cubic.lyapunov()}"
-    print(fstring)
+    #fstring = f"The average Lyapunov exponent is {cubic.lyapunov()}"
+    #print(fstring)
 
     ### plot results ###
     random = pm.draw_n(width, num_obj)
-    pm.plotresults(cubic.set_cubic, random, num_iter, num_seq)
+    #pm.plotresults(cubic.set_cubic, random, num_iter, num_seq)
+
+    # set up a set of cubic functions with lyapunov exponents in multiple regimes
+    len_cubic_set = 4
+    colors = ['black', 'r', 'b', 'g']
+    cubic_set = [pm.Cubic(cubicmap) for i in range(len_cubic_set)]
+    p_lists = []
+    for i in range(4):
+        x2, y2 = p_list[1]
+        p_lists.append([p_list[0], (x2, y2 + 0.25*i)])
+    coeffss = [pm.generate_cubic(p) for p in p_lists]
+    for i, cubic in enumerate(cubic_set):
+        cubic.set_coefficients(coeffss[i][0], coeffss[i][1], coeffss[i][2])
+
+    # plot phase maps and their local lyapunov distributions as inset
+    pm.plot_phasemaps(cubic_set, colors)
+
+    for i in range(4):
+        pm.plotresults(cubic_set[i].set_cubic, random, num_iter, num_seq)
 
     #hist_vals = [0]*100
 
--- phasemaps.py
+++ phasemaps.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 from sympy import *
+import seaborn as sns
 
 
 def draw_n(width, n=500):
@@ -48,7 +49,7 @@ def closest(i, j, k):
     d1 = j-i
     d2 = k-j
     if d1 == 0 or d2 == 0:
-        return 1e-30
+        return 1e-100
     elif d1 >= d2:
         return d2
     else:
@@ -67,9 +68,9 @@ def entropy(phases):
     res = 0
     for i in range(len_sp):
         if i == 0:
-            res += np.log(sp[1] - sp[0] + 1e-30)
+            res += np.log(sp[1] - sp[0] + 1e-100)
         elif i == len_sp - 1:
-            res += np.log(sp[-1] - sp[-2] + 1e-30)
+            res += np.log(sp[-1] - sp[-2] + 1e-100)
         else:
             res += np.log(closest(sp[i-1], sp[i], sp[i+1]))
     return res / len_sp + np.log(2*len_sp - 2) + np.euler_gamma
@@ -84,6 +85,38 @@ def evolve(f, phases, num_iter):
     return entropy_list
 
 
+def change(arr, before, after):
+    return arr[after] - arr[before]
+
+
+def average_entropy(arr):
+    """
+    compute the asymptotic entropy from the last 2-% of iterations
+
+    :param arr: num_seq by num_iter array of entropies
+    :return: 1 by num_iter array of average entropies
+    """
+    averages = np.mean(arr, axis=0)
+    asym_h = np.mean(averages[int(len(averages) * 0.8):])
+    print(f"The asymptotic entropy is approximately {asym_h}")
+    return averages, asym_h
+
+
+def plot_scatter(arr, averages, num_iter, n_seq, ax):
+    xs = np.linspace(1, num_iter, num=num_iter)
+    colors = cm.winter(np.linspace(0, 1, num=n_seq))
+    for i, c in enumerate(colors):
+        ax.scatter(xs, arr[i], color=c, s=1, alpha=0.3)
+    ax.plot(xs, averages, c='black')
+    ax.set_xlabel("iteration")
+    ax.set_ylabel("entropy")
+
+
+def plot_hist(q, ax, hist=True, kde=False, color=None, label=None):
+    sns.distplot(q, hist=hist, kde=kde, kde_kws={'shade': False, 'linewidth': 1}, ax=ax,
+                 color=color, label=label)
+
+
 def plotresults(f, phases, num_iter, n_seq):
     """
     plots evolution of entropy for an ensemble of objects
@@ -94,24 +127,59 @@ def plotresults(f, phases, num_iter, n_seq):
     :param n_seq: unsigned int, number of time-sequences to average over
     :return: void
     """
-    fig, ax1 = plt.subplots()
+    fig, ax1 = plt.subplots(nrows=2, ncols=3, figsize=(9,6))
     xs = np.linspace(1, num_iter, num=num_iter)
     ys = []
     for i in range(n_seq):
         res = evolve(f, phases, num_iter)
         ys.append(res)
-    np.array(ys).reshape((num_iter, n_seq))
-    averages = np.mean(ys, axis=0)
-    asym_h = np.mean(averages[int(len(averages) * 0.8):])
-    print(f"The asymptotic entropy is approximately {asym_h}")
-    colors = cm.spring(np.linspace(0, 1, n_seq))
-    for y, col in zip(ys, colors):
-        plt.scatter(xs, y, s=1, color=col, alpha=0.5)
-    plt.plot(xs, averages, c='black')
-    ax1.set_xlabel('iteration')
-    ax1.set_ylabel('entropy')
+    ys = np.array(ys).reshape((n_seq, num_iter))
+    ys_t = ys.T
+
+    # compute the asymptotic entropy from the last 20% iterations
+    averages, asym_h = average_entropy(ys)
+
+    # plot scatter of trajectories and average
+    plot_scatter(ys, averages, num_iter, n_seq, ax1[0][0])
+
+    abs_entropy = abs(ys_t[-1])
+    plot_hist(abs_entropy, ax1[0][1], True, True)
+    ax1[0][1].axvline(abs(asym_h), color='r', lw=1)
+    ax1[0][1].set_xlabel('absolute entropy')
+
+    # plot the change in entropy from 2nd to last to last iteration
+    entropy_change_last = change(ys_t, -1, -2)
+    plot_hist(entropy_change_last, ax1[0][2], True, True)
+    ax1[0][2].set_xlabel('change in entropy')
+
+    # plot lambda vs count
+    xss = np.linspace(0, 2*np.pi, num=10000)
+    slopes = local_slope(f, xss)
+    plot_hist(slopes, ax1[1][0], True, True)
+    ax1[1][0].set_xlabel('local lyapunov exponent')
+
+    # entropy vs. change in entropy at the final stage
+    indices = [-1, -31, -61]
+    scatter_color = ['black', 'red', 'blue']
+    for i, c in zip(indices, scatter_color):
+        ax1[1][1].scatter(ys_t[i-1], change(ys_t, i-1, i), color=c, s=1, label=f"n={int(xs[i])}")
+    ax1[1][1].legend(loc=0, prop={'size': 5})
+    ax1[1][1].set_xlabel('entropy $(n-1)^{th}$ iteration')
+    ax1[1][1].set_ylabel('change in entropy')
+
+    # delete extra set of axes
+    fig.delaxes(ax1[1][2])
+
+    plt.tight_layout()
     plt.show()
     return asym_h
+
+
+def local_slope(f, ps):
+    x = Symbol('x')
+    df = f(x).diff(x)
+    df = lambdify(x, df)
+    return [np.log(abs(df(p))) for p in ps]
 
 
 def gen(x_coord):
@@ -130,17 +198,43 @@ def generate_cubic(ps):
 
     :param p1: coordinate pair (x, y)
     :param p2: ^
-    :param p3: ^
     :return: tuple of (a, b, c)
     """
     rows = ()
     col = np.zeros(3)
+    ps.append((2*np.pi, 2*np.pi))
     for i, p in enumerate(ps):
         rows += (gen(p[0]),)
         col[i] = p[1]
     matrix = np.concatenate(rows, axis=0)
     coeffs = np.linalg.solve(matrix, col)
     return coeffs[0], coeffs[1], coeffs[2]
+
+
+def plot_phasemaps(cubic_set, colors):
+    fig, ax_main = plt.subplots()
+    ax = ax_main.inset_axes([.1, .7, .3, .3])
+    len_cubic_set = len(cubic_set)
+
+    # plot phase maps
+    for i in range(len_cubic_set):
+        cubic_set[i].show(ax_main, colors[i])
+    ax_main.set_xlabel(r"$\phi$")
+    ax_main.set_ylabel(r"$\psi(\phi)$")
+
+    # plot slope histograms as inset to phase maps
+    xss = np.linspace(0, 2*np.pi, num=10000)
+    for i in range(len(cubic_set)):
+        slopes = local_slope(cubic_set[i].set_cubic, xss)
+        avg_lyapunov = round(cubic_set[i].lyapunov(), 3)
+        label = f"{avg_lyapunov}"
+        plot_hist(slopes, ax, False, True, colors[i], label)
+        #ax.axvline(avg_lyapunov, color=colors[i], lw=1)
+    ax.set_xlabel(r"local lyapunov exponent $\lambda$", fontsize=5)
+    ax.tick_params(axis='x', labelsize=5)
+    ax.tick_params(axis='y', labelsize=5)
+    ax.legend(title=r'$\langle\lambda\rangle$', prop={'size': 5})
+    plt.show()
 
 
 class Cubic:
@@ -151,9 +245,11 @@ class Cubic:
     def __init__(self, f):
         self.f = f
 
-    @classmethod
-    def set_coefficients(cls, a, b, c, d=0):
-        cls.coefficients = (a, b, c, d)
+    def set_coefficients(self, a, b, c, d=0):
+        self.coefficients = (a, b, c, d)
+
+    def print_coefficients(self):
+        print(self.coefficients)
 
     def set_cubic(self, x):
         return self.f(coeffs=self.coefficients, x=x)
@@ -166,7 +262,6 @@ class Cubic:
         lyap = sum(np.log(abs(df(self.xs)))) * (1 / len(self.xs))
         return lyap
 
-    def show(self):
+    def show(self, ax, color='b'):
         f = self.set_cubic(self.xs)
-        plt.plot(self.xs, f, color='b')
-        plt.show()
+        ax.plot(self.xs, f, color=color)
