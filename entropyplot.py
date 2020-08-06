bd1ea44dabd510adb30c88bd26ce683ca1666dff
Tony <tonysong1111@gmail.com>
2020-08-06 10:52:57 -0400

    added plotting features

--- entropyplot.py
+++ entropyplot.py
@@ -18,6 +18,22 @@ def cubicmap(coeffs, x):
     return a*x**3 + b*x**2 + c*x + d
 
 
+def set_triangle(val):
+    def trianglemap(x):
+        res = []
+        for phase in x:
+            if phase <= np.pi:
+                res.append(phase)
+            elif phase <= val:
+                res.append(-phase + 2*np.pi)
+            else:
+                m3 = val / (2*np.pi - val)
+                b3 = 2*np.pi - (2*np.pi*val / (2*np.pi - val))
+                res.append(m3*phase + b3)
+        return res
+    return trianglemap
+
+
 def prompt_cubic():
     p_list = []
     line = input("Enter a list of coordinates, in the form of x y:\n")
@@ -39,37 +55,181 @@ def prompt_config():
 
 
 if __name__ == "__main__":
-    ### define model specifications ###
+    ### define model specifications from user###
     # p_list = prompt_cubic()
     # coeffs = pm.generate_cubic(p_list)
     # num_obj, num_iter, num_seq, width = prompt_config()
-    p_list = [(3.141, 2.356), (4.712, 1.385)]
-    coeffs = pm.generate_cubic(p_list)
-    num_obj, num_iter, num_seq, width = (300, 150, 50, 0.0001)
 
-    ### define cubic function ###
+    ### hard code values for checking
+    p_list = [(3.141, 2.356), (4.712, 0.5)]
+    #p_list = [(3.141, 2.356), (4.712, 1.385)]
+    #coeffs = pm.generate_cubic(p_list)
+
+    num_obj, num_iter, num_seq, width = (300, 300, 50, 0.0001)
+
+    ## define cubic function ###
     #cubic = pm.Cubic(cubicmap)
     #cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
 
-    ### print the lyapunov exponent ###
+    # show cubic function
+    # fig, ax = plt.subplots(ncols=2, nrows=2)
+    # x1, y1 = p_list[0]
+    # x2, y2 = p_list[1]
+    #
+    # increment = 0.55
+    # sub_axs = [ax[0][0], ax[0][1], ax[1][0], ax[1][1]]
+    # for i, sub_ax in enumerate(sub_axs):
+    #     p_list = [(x1, y1 - i * increment), (x2, y2 + i * increment)]
+    #     coeffs = pm.generate_cubic(p_list)
+    #     cubic = pm.Cubic(cubicmap)
+    #     cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
+    #     cubic.show(sub_ax)
+    #     sub_ax.plot([p_list[0][0], p_list[1][0]], [p_list[0][1], p_list[1][1]], 'ro')
+    #     label = rf"$\langle\lambda\rangle={round(cubic.lyapunov(),3)}$"
+    #     sub_ax.text(0.45, 0.8, label, fontsize=10, transform=sub_ax.transAxes)
+    # plt.savefig("graphics/phasemaps_four.png", bbox_inches='tight', dpi=1000)
+    # plt.show()
+
+    ## print the lyapunov exponent ###
     #fstring = f"The average Lyapunov exponent is {cubic.lyapunov()}"
     #print(fstring)
 
     ### plot results ###
+    # initialize some randome phases according to parameters
     random = pm.draw_n(width, num_obj)
-    #pm.plotresults(cubic.set_cubic, random, num_iter, num_seq)
+
+    # make one plot (regular)
+    # fig, ax = plt.subplots()
+    # ys, _ = pm.load_results(cubic.set_cubic, random, num_iter, num_seq)
+    # averages, avg_H = pm.average_entropy(ys    fig, ax = plt.subplots()
+    # pm.plot_scatter(ys, averages, num_iter, num_seq, ax)
+    # ax.text(0.5, 0.17, rf"$\langle\lambda\rangle={round(cubic.lyapunov(), 3)}$", fontsize=15,
+    #         transform=ax.transAxes)
+    # plt.savefig("graphics/lyap0.141_fullstart.png", bbox_inches='tight', dpi=1000)
+    # plt.show()
+
+
+    ### plot for same phase map but different starting points
+    # fig, ax = plt.subplots()
+    # xs = np.linspace(1, num_iter, num=num_iter)
+    #
+    # objects_full = pm.draw_n(0.0001, num_obj)
+    # objects_concentrated = pm.draw_n(2*np.pi, num_obj)
+    # ys1, body_phases1 = pm.load_results(cubic.set_cubic, objects_full,
+    #                                     num_iter, num_seq)
+    # ys2, body_phases2 = pm.load_results(cubic.set_cubic, objects_concentrated,
+    #                                     num_iter, num_seq)
+    # averages1, asym_H1 = pm.average_entropy(ys1)
+    # averages2, asym_H2 = pm.average_entropy(ys2)
+    # ax.plot(xs, averages1, c='b', label=r'$w=$1e-4')
+    # ax.plot(xs, averages2, c='r', label=r'$w=2\pi$')
+    # ax.text(0.65, 0.68, rf'$\langle H\rangle_\infty={round(asym_H2, 3)}$', color='r',
+    #         transform=ax.transAxes)
+    # ax.text(0.65, 0.475, rf'$\langle H\rangle_\infty={round(asym_H1, 3)}$', color='b',
+    #         transform=ax.transAxes)
+    # plt.legend()
+    # #plt.savefig("graphics/different_starting.png", bbox_inches='tight', dpi=1000)
+    # plt.show()
+
+
+
+
+    ### plot for triangle phase map
+    # phasemap1 = set_triangle(3.669)
+    # phasemap2 = set_triangle(5.989)
+    # fig, ax = plt.subplots(nrows=2, ncols=2)
+    #
+    # ys1, body_phases1 = pm.load_results(phasemap1, random, num_iter, num_seq)
+    # averages1, asym_H1 = pm.average_entropy(ys1)
+    # ys2, body_phases2 = pm.load_results(phasemap2, random, num_iter, num_seq)
+    # averages2, asym_H2 = pm.average_entropy(ys2)
+    #
+    # xs = np.linspace(0, 2*np.pi, num=10000)
+    #
+    # ax[0][0].plot(xs, phasemap1(xs), c='b', lw=2)
+    # ax[0][0].text(0.1, 0.9, r"$\psi(\phi)$ with $\langle\lambda\rangle=0.141$",
+    #               transform=ax[0][0].transAxes, size=8)
+    # pm.plot_scatter(ys1, averages1, num_iter, num_seq, ax[0][1])
+    # ax[0][1].text(0.7, 0.1, rf"$\langle H\rangle_\infty=${round(asym_H1,3)}",
+    #               transform=ax[0][1].transAxes, size=8)
+    #
+    # ax[1][0].plot(xs, phasemap2(xs), c='b', lw=2)
+    # pm.plot_scatter(ys2, averages2, num_iter, num_seq, ax[1][1])
+    # ax[1][1].text(0.7, 0.1, rf"$\langle H\rangle_\infty=${round(asym_H2, 3)}",
+    #               transform=ax[1][1].transAxes, size=8)
+    #
+    # for sub_ax in [ax[0][0], ax[1][0]]:
+    #     sub_ax.set_aspect("equal")
+    #
+    # fig.tight_layout()
+    # #plt.savefig("graphics/trianglemap_comparison.png", bbox_inches='tight', dpi=1000)
+    # plt.show()
+
+    #lambda vs entropy
+    # fig, ax = plt.subplots()
+    # num = 350
+    # lambdas = [0]*num
+    # asym_hs = [0]*num
+    # increment = 0.005
+    # x1, y1 = p_list[0]
+    # x2, y2 = p_list[1]
+    # cubic = pm.Cubic(cubicmap)
+    # for j in range(num):
+    #     p_list = [(x1, y1 - j * increment), (x2, y2 + j * increment)]
+    #     coeffs = pm.generate_cubic(p_list)
+    #     cubic = pm.Cubic(cubicmap)
+    #     cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
+    #     lambdas[j] = cubic.lyapunov()
+    #     ys, _ = pm.load_results(cubic.set_cubic, random, num_iter, num_seq)
+    #     _, asym_hs[j] = pm.average_entropy(ys)
+    #     print(j)
+    # ax.scatter(lambdas, asym_hs, color='b', s=5)
+    # ax.set_xlabel(r"$\langle\lambda\rangle$")
+    # ax.set_ylabel(r"$\langle H\rangle_\infty$")
+    # plt.savefig("graphics/big_lambdaVSentropy_300iter.png", bbox_inches='tight', dpi=1000)
+    # plt.show()
+
+
+    #pm.plot_results(cubic.set_cubic, random, num_iter, num_seq)
+
+    # plot phase circles for individual trajectories
+    # p_list = [(3.141, 2.356), (4.712, 1.385+0.25)]
+    # coeffs = pm.generate_cubic(p_list)
+    #
+    # cubic = pm.Cubic(cubicmap)
+    # cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
+    #
+    fig, ax = plt.subplots(ncols=2, nrows=2)
+    ax_list = [ax[0][0], ax[0][1], ax[1][0], ax[1][1]]
+    np.random.seed(1)
+    x1, y1 = p_list[0]
+    x2, y2 = p_list[1]
+
+    increment = 0.3
+    sub_axs = [ax[0][0], ax[0][1], ax[1][0], ax[1][1]]
+    for i, sub_ax in enumerate(sub_axs):
+        p_list = [(x1, y1 - i * increment), (x2, y2 + i * increment)]
+        coeffs = pm.generate_cubic(p_list)
+        cubic = pm.Cubic(cubicmap)
+        cubic.set_coefficients(coeffs[0], coeffs[1], coeffs[2])
+        pm.plot_phases(cubic.set_cubic, random, num_iter, num_seq, sub_ax)
+        sub_ax.text(0.45, 0.1, rf"$\langle\lambda\rangle={round(cubic.lyapunov(), 3)}$",
+                    fontsize=10, transform=sub_ax.transAxes)
+    #plt.suptitle(rf"$\langle\lambda\rangle=${round(cubic.lyapunov(), 3)}")
+    plt.savefig("graphics/lostpoints_manylyapunov.png", bbox_inches='tight', dpi=1000)
+    plt.show()
 
     # set up a set of cubic functions with lyapunov exponents in multiple regimes
-    len_cubic_set = 4
-    colors = ['black', 'r', 'b', 'g']
-    cubic_set = [pm.Cubic(cubicmap) for i in range(len_cubic_set)]
-    p_lists = []
-    for i in range(4):
-        x2, y2 = p_list[1]
-        p_lists.append([p_list[0], (x2, y2 + 0.25*i)])
-    coeffss = [pm.generate_cubic(p) for p in p_lists]
-    for i, cubic in enumerate(cubic_set):
-        cubic.set_coefficients(coeffss[i][0], coeffss[i][1], coeffss[i][2])
+    # len_cubic_set = 4
+    # colors = ['black', 'r', 'b', 'g']
+    # cubic_set = [pm.Cubic(cubicmap) for i in range(len_cubic_set)]
+    # p_lists = []
+    # for i in range(4):
+    #     x2, y2 = p_list[1]
+    #     p_lists.append([p_list[0], (x2, y2 + 0.25*i)])
+    # coeffss = [pm.generate_cubic(p) for p in p_lists]
+    # for i, cubic in enumerate(cubic_set):
+    #     cubic.set_coefficients(coeffss[i][0], coeffss[i][1], coeffss[i][2])
 
     # plot phase maps and their local lyapunov distributions as inset
     #pm.plot_phasemaps(cubic_set, colors)
@@ -89,6 +249,24 @@ if __name__ == "__main__":
     # plot phases distributed over a circle
     #pm.plot_circle(np.random.uniform(0, 1, 300))
 
-    # plot animation
-    pm.animate(cubic_set[0].set_cubic, random, num_iter)
-    plt.show()
+    #pm.plot_decay2(cubic.set_cubic)
+
+    #see what happens starting from uniform
+    # ncols = 10
+    # nrows = 10
+    # phase_list, entropy_list = pm.evolve(phasemap1, random, ncols*nrows)
+    # #print(entropy_list)
+    # #print(coeffs)
+    # fig, ax = plt.subplots(nrows=nrows, ncols=ncols)
+    # for i in range(nrows):
+    #     for j in range(ncols):
+    #         pm.plot_circle(phase_list[ncols*i + j], ax[i][j])
+    #         ax[i][j].axis('off')
+    #         ax[i][j].text(0, 0, f'n = {ncols*i + j + 1}', fontsize=4.5,
+    #                       ha='center', va='center')
+    # #r_coeffs = [round(c, 3) for c in coeffs]
+    # #title = rf"$\psi(\phi)=${r_coeffs[0]}$\phi^3-${abs(r_coeffs[1])}$\phi^2+${r_coeffs[2]}$\phi,\quad\lambda=${round(cubic.lyapunov(), 3)}"
+    # title = r"triangle map with $\langle\lambda\rangle=0.141$ (top row map)"
+    # plt.suptitle(title)
+    # #plt.savefig('graphics/trianglemap_fig100.png', bbox_inches='tight', dpi=1000)
+    # plt.show()
--- phasemaps.py
+++ phasemaps.py
@@ -5,10 +5,11 @@ Updated July 2, 2020
 """
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import matplotlib.animation as animation
 import numpy as np
 from sympy import *
 import seaborn as sns
+from scipy.optimize import curve_fit
+from collections import Counter
 
 
 def draw_n(width, n=500):
@@ -20,7 +21,7 @@ def draw_n(width, n=500):
     :param n: unsigned integer, number of objects (default 500 if not specified)
     :return: np array of phases
     """
-    np.random.seed(2)
+    np.random.seed(3)
     left = np.random.uniform(0, 2*np.pi, 1)
     arr = np.array(np.random.uniform(left, left+width, n))
     return np.mod(arr, 2*np.pi)
@@ -50,7 +51,7 @@ def closest(i, j, k):
     d1 = j-i
     d2 = k-j
     if d1 == 0 or d2 == 0:
-        return 1e-30
+        return 1e-15
     elif d1 >= d2:
         return d2
     else:
@@ -69,9 +70,9 @@ def entropy(phases):
     res = 0
     for i in range(len_sp):
         if i == 0:
-            res += np.log(sp[1] - sp[0] + 1e-30)
+            res += np.log(sp[1] - sp[0] + 1e-15)
         elif i == len_sp - 1:
-            res += np.log(sp[-1] - sp[-2] + 1e-30)
+            res += np.log(sp[-1] - sp[-2] + 1e-15)
         else:
             res += np.log(closest(sp[i-1], sp[i], sp[i+1]))
     return res / len_sp + np.log(2*len_sp - 2) + np.euler_gamma
@@ -79,13 +80,14 @@ def entropy(phases):
 
 def evolve(f, phases, num_iter):
     entropy_list = np.zeros(num_iter+1)
-    phase_list = np.array([phases])
+    num_bodies = len(phases)
+    phase_list = np.array([[0]*num_bodies])
     for i in range(num_iter+1):
         entropy_list[i] = entropy(phases)
         phases = iterate(f, phases)
-        np.append(phase_list, [phases], axis=0)
+        phase_list = np.append(phase_list, np.array([phases]), axis=0)
     entropy_list = np.delete(entropy_list, 0)
-    phase_list = np.delete(phase_list, 0)
+    phase_list = phase_list[1:-1].reshape((num_iter, num_bodies))
     return phase_list, entropy_list
 
 
@@ -95,7 +97,7 @@ def change(arr, before, after):
 
 def average_entropy(arr):
     """
-    compute the asymptotic entropy from the last 2-% of iterations
+    compute the asymptotic entropy from the last 20% of iterations
 
     :param arr: num_seq by num_iter array of entropies
     :return: 1 by num_iter array of average entropies
@@ -110,10 +112,10 @@ def plot_scatter(arr, averages, num_iter, n_seq, ax):
     xs = np.linspace(1, num_iter, num=num_iter)
     colors = cm.winter(np.linspace(0, 1, num=n_seq))
     for i, c in enumerate(colors):
-        ax.scatter(xs, arr[i], color=c, s=1, alpha=0.3)
+        ax.scatter(xs, arr[i], color=c, s=1, alpha=0.2)
     ax.plot(xs, averages, c='black')
-    ax.set_xlabel("iteration")
-    ax.set_ylabel("entropy")
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Entropy")
 
 
 def plot_hist(q, ax, hist=True, kde=False, color=None, label=None):
@@ -126,10 +128,9 @@ def plot_circle(phases, ax):
     ys = np.sin(phases)
 
     xss = np.linspace(0, 2*np.pi, num=10000)
-    ax.scatter(xs, ys, c='r', marker='o', alpha = 0.3, zorder=10)
+    ax.scatter(xs, ys, c='r', marker='o', alpha = 0.1, zorder=10, s=3)
     ax.plot(np.cos(xss), np.sin(xss), color='black', lw=1, zorder=0)
     ax.set_aspect('equal', 'box')
-    plt.show()
     return xs, ys
 
 
@@ -138,15 +139,76 @@ def phase_evolution(f, phases, num_iter):
     return phase_list
 
 
-# animation
-def animate(f, phases, num_iter):
-    fig, ax = plt.subplots()
-    phase_list = phase_evolution(f, phases, num_iter)
-    return animation.FuncAnimation(fig, plot_circle, frames=phase_list,
-                                   fargs=(ax,))
+def load_results(f, phases, num_iter, n_seq):
+    ys = []
+    body_phases = []
+
+    for i in range(n_seq):
+        phase_list, res = evolve(f, phases, num_iter)
+        ys.append(res)
+        body_phases.append(phase_list)
+    ys = np.array(ys).reshape((n_seq, num_iter))
+    body_phases = np.array(body_phases)
+    return ys, body_phases
 
 
-def plotresults(f, phases, num_iter, n_seq):
+def plot_phases(f, phases, num_iter, n_seq, ax):
+    """
+    loads data for n_seq of time sequences
+    this function plots only one of those time sequences
+    """
+    # load the data
+    ys, body_phase = load_results(f, phases, num_iter, n_seq)
+    selection = [int(num_iter * (1 - 0.05 * i)) - 1 for i in range(10)]
+
+    # plot one trajectory and select phase distributions
+    iteration = int(np.random.uniform(1, n_seq, 1))
+    xs = np.linspace(1, num_iter, num_iter)
+    count_distinct_phases = [count_distinct(body_phase[iteration][i]) for i in range(num_iter)]
+
+    s = count_distinct_phases[0]
+    for i in range(len(count_distinct_phases)):
+        if i == 0:
+            pass
+        elif count_distinct_phases[i] == s:
+            count_distinct_phases[i] = -9999
+        else:
+            s = count_distinct_phases[i]
+
+    ax.plot(xs, ys[iteration], '-o', c='b', alpha=0.3, linewidth=1,
+            markersize=2, zorder=0)
+    ax.scatter([xs[i] for i in selection], [ys[iteration, i] for i in selection],
+               color='r', s=2, zorder=5)
+    for i, count in enumerate(count_distinct_phases):
+        if count == -9999:
+            pass
+        else:
+            ax.annotate(count, (xs[i], ys[iteration][i]), fontsize=4)
+
+    # ax_inset1 = ax.inset_axes([0.15, 0.25, 0.15, 0.15], zorder=10)
+    # ax_inset2 = ax.inset_axes([0.30, 0.25, 0.15, 0.15], zorder=10)
+    # ax_inset3 = ax.inset_axes([0.45, 0.25, 0.15, 0.15], zorder=10)
+    # ax_inset4 = ax.inset_axes([0.60, 0.25, 0.15, 0.15], zorder=10)
+    # ax_inset5 = ax.inset_axes([0.75, 0.25, 0.15, 0.15], zorder=10)
+    # ax_inset6 = ax.inset_axes([0.15, 0.05, 0.15, 0.15], zorder=10)
+    # ax_inset7 = ax.inset_axes([0.30, 0.05, 0.15, 0.15], zorder=10)
+    # ax_inset8 = ax.inset_axes([0.45, 0.05, 0.15, 0.15], zorder=10)
+    # ax_inset9 = ax.inset_axes([0.60, 0.05, 0.15, 0.15], zorder=10)
+    # ax_inset10 = ax.inset_axes([0.75, 0.05, 0.15, 0.15], zorder=10)
+    # inset_dict = {'inset1': ax_inset1, 'inset2': ax_inset2, 'inset3': ax_inset3, 'inset4': ax_inset4,
+    #               'inset5': ax_inset5, 'inset6': ax_inset6, 'inset7': ax_inset7, 'inset8': ax_inset8,
+    #               'inset9': ax_inset9, 'inset10': ax_inset10}
+    # inset_dict_keys = ['inset1', 'inset2', 'inset3', 'inset4', 'inset5', 'inset6', 'inset7',
+    #                    'inset8', 'inset9', 'inset10']
+    #
+    # for i, key in enumerate(inset_dict_keys):
+    #     plot_circle(body_phase[iteration, selection[i]], inset_dict[key])
+    #     inset_dict[key].text(0, 0, f'n = {selection[i] + 1}', fontsize=4.5,
+    #                          ha='center', va='center')
+    #     inset_dict[key].axis('off')
+
+
+def plot_results(f, phases, num_iter, n_seq):
     """
     plots evolution of entropy for an ensemble of objects
 
@@ -156,13 +218,12 @@ def plotresults(f, phases, num_iter, n_seq):
     :param n_seq: unsigned int, number of time-sequences to average over
     :return: void
     """
+    # set up the axes
     fig, ax1 = plt.subplots(nrows=2, ncols=3, figsize=(9,6))
+
+    # load entropy evolution data
+    ys, body_phases = load_results(f, phases, num_iter, n_seq)
     xs = np.linspace(1, num_iter, num=num_iter)
-    ys = []
-    for i in range(n_seq):
-        phase_list, res = evolve(f, phases, num_iter)
-        ys.append(res)
-    ys = np.array(ys).reshape((n_seq, num_iter))
     ys_t = ys.T
 
     # compute the asymptotic entropy from the last 20% iterations
@@ -171,30 +232,34 @@ def plotresults(f, phases, num_iter, n_seq):
     # plot scatter of trajectories and average
     plot_scatter(ys, averages, num_iter, n_seq, ax1[0][0])
 
-    abs_entropy = abs(ys_t[-1])
-    plot_hist(abs_entropy, ax1[0][1], True, True)
-    ax1[0][1].axvline(abs(asym_h), color='r', lw=1)
-    ax1[0][1].set_xlabel('absolute entropy')
+    # # plot absolute entropy
+    # abs_entropy = abs(ys_t[-1])
+    # plot_hist(abs_entropy, ax1[0][1], True, True)
+    # ax1[0][1].axvline(abs(asym_h), color='r', lw=1)
+    # ax1[0][1].set_xlabel('absolute entropy')
+    #
+    # # plot the change in entropy from 2nd to last to last iteration
+    # entropy_change_last = change(ys_t, -1, -2)
+    # plot_hist(entropy_change_last, ax1[0][2], True, True)
+    # ax1[0][2].set_xlabel('change in entropy')
+    #
+    # # plot lambda vs count
+    # xss = np.linspace(0, 2*np.pi, num=10000)
+    # slopes = local_slope(f, xss)
+    # plot_hist(slopes, ax1[1][0], True, True)
+    # ax1[1][0].set_xlabel('local lyapunov exponent')
+    #
+    # # entropy vs. change in entropy at the final stage
+    # indices = [-1, -31, -61]
+    # scatter_color = ['black', 'red', 'blue']
+    # for i, c in zip(indices, scatter_color):
+    #     ax1[1][1].scatter(ys_t[i-1], change(ys_t, i-1, i), color=c, s=1, label=f"n={int(xs[i])}")
+    # ax1[1][1].legend(loc=0, prop={'size': 5})
+    # ax1[1][1].set_xlabel('entropy $(n-1)^{th}$ iteration')
+    # ax1[1][1].set_ylabel('change in entropy')
 
-    # plot the change in entropy from 2nd to last to last iteration
-    entropy_change_last = change(ys_t, -1, -2)
-    plot_hist(entropy_change_last, ax1[0][2], True, True)
-    ax1[0][2].set_xlabel('change in entropy')
-
-    # plot lambda vs count
-    xss = np.linspace(0, 2*np.pi, num=10000)
-    slopes = local_slope(f, xss)
-    plot_hist(slopes, ax1[1][0], True, True)
-    ax1[1][0].set_xlabel('local lyapunov exponent')
-
-    # entropy vs. change in entropy at the final stage
-    indices = [-1, -31, -61]
-    scatter_color = ['black', 'red', 'blue']
-    for i, c in zip(indices, scatter_color):
-        ax1[1][1].scatter(ys_t[i-1], change(ys_t, i-1, i), color=c, s=1, label=f"n={int(xs[i])}")
-    ax1[1][1].legend(loc=0, prop={'size': 5})
-    ax1[1][1].set_xlabel('entropy $(n-1)^{th}$ iteration')
-    ax1[1][1].set_ylabel('change in entropy')
+    # plot just one circle from the data
+    #plot_circle(body_phases[10, -10], ax1[1][2])
 
     # delete extra set of axes
     fig.delaxes(ax1[1][2])
@@ -263,6 +328,69 @@ def plot_phasemaps(cubic_set, colors):
     ax.tick_params(axis='x', labelsize=5)
     ax.tick_params(axis='y', labelsize=5)
     ax.legend(title=r'$\langle\lambda\rangle$', prop={'size': 5})
+    plt.show()
+
+
+def count_distinct(arr):
+    """
+
+    :param arr: array of phases
+    :return: integer, number of distinct values
+    """
+    return len(Counter(arr).keys())
+
+
+def plot_decay(f, step_size=0.0001):
+    xs = np.linspace(0, 2*np.pi, num=100000)
+
+    left = -10
+    right = 3
+    lambdas = np.linspace(left, right, num=int((right - left)/step_size))
+    slopes = local_slope(f, xs)
+    density = np.zeros(len(lambdas) - 1)
+    for i in range(len(lambdas) - 1):
+        count = 0
+        for slope in slopes:
+            if lambdas[i + 1] >= slope >= lambdas[i]:
+                count += 1
+        density[i] = count
+
+    fig, ax = plt.subplots()
+    ax.plot(lambdas[:-1], density, c='b', lw=1)
+    plt.show()
+
+
+def inv_sq_fit(x, a, b):
+    return a / (x**2) + b
+
+
+def inv_sq_decrease(x, e):
+    return 1/(x**2) - 1/(x-e)**2
+
+def plot_decay2(f):
+    fig, ax = plt.subplots()
+    xs = np.linspace(0, 2*np.pi, num=100000)
+    slopes = local_slope(f, xs)
+    xs, ys = sns.distplot(slopes, hist=False, kde=True,
+                          kde_kws={'shade': False, 'linewidth': 2},
+                          color='b').get_lines()[0].get_data()
+
+    peak = np.argmax(ys)
+    xs_d = []
+    ys_d = []
+    increment = 0.05
+    x = xs[peak-10]
+    y = ys[peak-10]
+    xs_d = []
+    ys_d = []
+    for i in range(10):
+        y -= inv_sq_decrease(x, increment)
+        x -= increment
+        xs_d.append(x)
+        ys_d.append(y)
+    ax.scatter(xs_d, ys_d, color='r', s=15, zorder=10)
+    # params, params_cov = curve_fit(inv_sq_fit, x_data, y_data)
+    # ax.plot(x_data, inv_sq_fit(x_data, params[0], params[1]))
     plt.show()
 
 
